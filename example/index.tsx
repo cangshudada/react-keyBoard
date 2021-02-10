c6cdaff42f6d3b5affbcebce41cd2400e9c29ea8
cangshudada <15342240569@163.com>
2021-02-10 16:19:08 +0800

    project init

---
+++ example/index.tsx
@@ -0,0 +1,51 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import "./index.less";
+import { KeyBoard } from '../.';
+
+const App = () => {
+  return (
+    <div>
+      <div className="demo">
+        <form>
+          <div className="form-group">
+            <label>默认</label>
+            <input data-mode className="form-control" v-model="value" />
+          </div>
+          <div className="form-group">
+            <label>英文键盘</label>
+            <input data-mode="en" className="form-control" v-model="value" />
+          </div>
+          <div className="form-group">
+            <label>数字键盘</label>
+            <input
+              data-mode="number"
+              className="form-control"
+              v-model="value"
+            />
+          </div>
+          <div className="form-group">
+            <label>标点键盘</label>
+            <input
+              data-mode="symbol"
+              className="form-control"
+              v-model="value"
+            />
+          </div>
+          <div className="form-group">
+            <label>手写键盘</label>
+            <input
+              data-mode="handwrite"
+              className="form-control"
+              v-model="value"
+            />
+          </div>
+        </form>
+        <KeyBoard />
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+// Delete me
+export const KeyBoard = () => {
+  return <div>qwe</div>;
+};
